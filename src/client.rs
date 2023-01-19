3f31abe5e43c38b611ac714ade0c7ebc46f947f2
Chiro Liang <Chiro2001@163.com>
2023-01-19 22:40:16 +0800

    clean deps

--- src/client.rs
+++ src/client.rs
@@ -54,13 +54,58 @@ impl ChatinoClient {
 
 #[cfg(not(target_arch = "wasm32"))]
 mod test {
-    use crate::client::ChatinoClient;
+    use crate::client::{ChatinoClient, PORT, PROTOCOL, SERVER};
     use anyhow::Result;
-    use futures_util::StreamExt;
+    use futures_util::{future, pin_mut, StreamExt};
     use log::{error, info, warn};
     use std::time::Duration;
+    use tokio::io::{AsyncReadExt, AsyncWriteExt};
     use tokio::time::sleep;
+    use tokio_tungstenite::connect_async;
     use tokio_tungstenite::tungstenite::Message;
+
+    async fn read_stdin(tx: futures_channel::mpsc::UnboundedSender<Message>) {
+        let mut stdin = tokio::io::stdin();
+        loop {
+            let mut buf = vec![0; 1024];
+            let n = match stdin.read(&mut buf).await {
+                Err(_) | Ok(0) => break,
+                Ok(n) => n,
+            };
+            buf.truncate(n);
+            tx.unbounded_send(Message::binary(buf)).unwrap();
+        }
+    }
+
+    #[test]
+    fn test_websocket() -> Result<()> {
+        let runner = async {
+            // let url = format!("{}://{}:{}/", PROTOCOL, SERVER, PORT);
+            let url = format!("{}://{}:{}/", "ws", "localhost", 12345);
+            let (stdin_tx, stdin_rx) = futures_channel::mpsc::unbounded();
+            tokio::spawn(read_stdin(stdin_tx));
+
+            let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
+            println!("WebSocket handshake has been successfully completed");
+
+            let (write, read) = ws_stream.split();
+
+            let stdin_to_ws = stdin_rx.map(Ok).forward(write);
+            let ws_to_stdout = {
+                read.for_each(|message| async {
+                    let data = message.unwrap().into_data();
+                    tokio::io::stdout().write_all(&data).await.unwrap();
+                })
+            };
+
+            pin_mut!(stdin_to_ws, ws_to_stdout);
+            future::select(stdin_to_ws, ws_to_stdout).await;
+        };
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        rt.block_on(runner);
+        warn!("progress exited");
+        Ok(())
+    }
 
     #[test]
     fn test_client() -> Result<()> {
@@ -71,16 +116,18 @@ mod test {
             let reader = r.reader;
             let ws_to_client = async {
                 sleep(Duration::from_millis(1700)).await;
-                reader.for_each(|message| async {
-                    match message {
-                        Ok(message) => {
-                            println!("recv message: {}", message)
-                        }
-                        Err(e) => {
-                            error!("err on connection: {}", e)
-                        }
-                    };
-                }).await;
+                reader
+                    .for_each(|message| async {
+                        match message {
+                            Ok(message) => {
+                                println!("recv message: {}", message)
+                            }
+                            Err(e) => {
+                                error!("err on connection: {}", e)
+                            }
+                        };
+                    })
+                    .await;
                 warn!("reader to stdout exited");
             };
             tokio::spawn(ws_to_client);
