93652f1c6793d8880bd9e52e4121eb11edb00c27
Chiro Liang <Chiro2001@163.com>
2023-01-19 22:06:35 +0800

    trying

--- src/client.rs
+++ src/client.rs
@@ -27,59 +27,20 @@ pub struct ChatinoClient {
 
 #[cfg(not(target_arch = "wasm32"))]
 impl ChatinoClient {
-    pub async fn new(
-        // send_tx: &UnboundedSender<Message>,
-        send_rx: UnboundedReceiver<Message>,
-        // recv_tx: UnboundedSender<Message>,
-        // recv_rx: &UnboundedReceiver<Message>,
-    ) -> Result<Self> {
+    pub async fn new(sender: UnboundedReceiver<Message>) -> Result<Self> {
         let protocol = PROTOCOL.to_string();
         let server = SERVER.to_string();
         let port = PORT;
         let url = format!("{}://{}:{}/", protocol, server, port);
-        // let (send_tx, send_rx) = futures_channel::mpsc::unbounded();
-        // let (recv_tx, recv_rx) = futures_channel::mpsc::unbounded();
-        // ws.send_text("{\"cmd\":\"getinfo\"}".to_string()).await?;
         let (ws_stream, _) = connect_async(url).await.expect("failed to connect");
-        let (write, read) = ws_stream.split();
-
-        let client_to_ws = send_rx.map(Ok).forward(write);
-        // let arc_recv_tx = Arc::new(recv_tx);
-        // let ws_to_client = {
-        //     // let recv_tx = arc_recv_tx.clone();
-        //     read.for_each(|message| async {
-        //         match message {
-        //             Ok(message) => match recv_tx.unbounded_send(message) {
-        //                 Ok(_) => {}
-        //                 Err(e) => {
-        //                     error!("recv_tx err: {}", e)
-        //                 }
-        //             },
-        //             Err(e) => {
-        //                 error!("err on connection: {}", e)
-        //             }
-        //         };
-        //     })
-        // };
-        // let f = || {
-        //     // a.await;
-        //     tokio::spawn(client_to_ws);
-        //     tokio::spawn(ws_to_client);
-        //     ()
-        // };
-        // tokio::spawn(future::select(client_to_ws, ws_to_client));
+        let (write, reader) = ws_stream.split();
+        let client_to_ws = sender.map(Ok).forward(write);
         tokio::spawn(client_to_ws);
-        // tokio::spawn(ws_to_client);
         Ok(Self {
             protocol,
             server,
             port,
-            // send_tx,
-            // send_rx,
-            // recv_rx,
-            // recv_tx,
-            // runner: Box::new(f),
-            reader: read,
+            reader,
         })
     }
 }
@@ -89,17 +50,20 @@ mod test {
     use crate::client::ChatinoClient;
     use anyhow::Result;
     use futures_util::StreamExt;
-    use log::error;
+    use log::{error, info};
+    use std::time::Duration;
+    use tokio::time::sleep;
+    use tokio_tungstenite::tungstenite::Message;
 
     #[test]
     fn test_client() -> Result<()> {
-        let (send_tx, send_rx) = futures_channel::mpsc::unbounded();
+        tracing_subscriber::fmt::init();
+        let (sender, send_rx) = futures_channel::mpsc::unbounded();
         let runner = async {
             let r = ChatinoClient::new(send_rx).await.unwrap();
-            let read = r.reader;
+            let reader = r.reader;
             let ws_to_client = {
-                // let recv_tx = arc_recv_tx.clone();
-                read.for_each(|message| async {
+                reader.for_each(|message| async {
                     match message {
                         Ok(message) => {
                             println!("recv message: {}", message)
@@ -111,6 +75,17 @@ mod test {
                 })
             };
             tokio::spawn(ws_to_client);
+            loop {
+                sleep(Duration::from_millis(1000)).await;
+                match sender.unbounded_send(Message::Text("{\"cmd\":\"getinfo\"}".to_string())) {
+                    Ok(_) => {
+                        info!("message sent")
+                    }
+                    Err(e) => {
+                        error!("sender error: {}", e)
+                    }
+                }
+            }
         };
         let rt = tokio::runtime::Runtime::new().unwrap();
         rt.block_on(runner);
